d162444d6b071af86f46586dc7748b98d6ab2c84
sinhaarijit025 <sinhaarijit012@gmail.com>
2026-06-25 11:58:15 +0530

    Update Sign_Extend.v

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -9,7 +9,7 @@ module Sign_Extend (In, Imm_Ext, ImmSrc);
             2'b00: Imm_Ext = {{20{In[31]}}, In[31:20]}; // I-type
             2'b01: Imm_Ext = {{20{In[31]}}, In[31:25], In[11:7]}; // S-type
             2'b10: Imm_Ext = {{19{In[31]}}, In[31], In[7], In[30:25], In[11:8], 1'b0}; // B-type
-            2'b11: Imm_Ext = {{11{In[31]}}, In[31], In[19:12], In[20], In[30:21], 1'b0}; // J-type ✅
+            2'b11: Imm_Ext = {{11{In[31]}}, In[31], In[19:12], In[20], In[30:21], 1'b0}; // J-type 
         endcase
     end
 
